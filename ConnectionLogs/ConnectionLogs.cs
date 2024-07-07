f51592df6ae9be9ccc622b2ee75feb31d1ccd664
MADNESSZ1RA <92206761+MADNESSZ1RA@users.noreply.github.com>
2024-07-07 21:01:36 +0300

    Update ConnectionLogs.cs

--- ConnectionLogs/ConnectionLogs.cs
+++ ConnectionLogs/ConnectionLogs.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Text.Json;
@@ -7,141 +8,183 @@ using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
 using Microsoft.Extensions.Logging;
 using MySqlConnector;
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core.Attributes;
+using CounterStrikeSharp.API.Modules.Events;
 
-
-namespace HelloWorldPlugin;
-public class HelloWorldPlugin : BasePlugin
+namespace HelloWorldPlugin
 {
-    public override string ModuleAuthor => "ZIRA";
-    public override string ModuleName => "[Discord] Pro Connection Logs";
-    public override string ModuleVersion => "v1.2";
-
-    private Config _config = null!;
-
-    public override void Load(bool hotReload)
+    public class HelloWorldPlugin : BasePlugin
     {
-        _config = LoadConfig();
-    }
-    [GameEventHandler]
-    public HookResult OnPlayerConnectFull(EventPlayerConnectFull @event, GameEventInfo info)
-    {
-        if (@event.Userid != null && @event.Userid.IsValid)
+        public override string ModuleAuthor => "ZIRA";
+        public override string ModuleName => "[Discord] Pro Connection Logs";
+        public override string ModuleVersion => "v2.0";
+
+        private Config _config = null!;
+        private readonly Dictionary<int, DateTime> _playerConnectTimes = new();
+
+        public override void Load(bool hotReload)
         {
-            var _PlayerName = @event.Userid.PlayerName;
-            var _PlayerSteamId32 = ((int)@event.Userid.SteamID);
-            var _PlayerSteamId64 = @event.Userid.SteamID;
-            var _AdminStatus = CheckAdminStatus(SteamId32To64(_PlayerSteamId32));
-            var _VIPStatus = CheckVipStatus(_PlayerSteamId32);
+            _config = LoadConfig();
+            RegisterListener<Listeners.OnClientDisconnect>(OnClientDisconnect);
+        }
 
-            if (@event.Userid.IsValid)
+        [GameEventHandler]
+        public HookResult OnPlayerConnectFull(EventPlayerConnectFull @event, GameEventInfo info)
+        {
+            if (@event.Userid != null && @event.Userid.IsValid)
             {
-                Logger.LogInformation("NAME = " + _PlayerName);
-                Logger.LogInformation("ID32 = " + _PlayerSteamId32);
-                Logger.LogInformation("ID64 = " + _PlayerSteamId64);
+                var playerName = @event.Userid.PlayerName;
+                var playerSteamId32 = (int)@event.Userid.SteamID;
+                var playerSteamId64 = @event.Userid.SteamID;
+                var adminStatus = CheckAdminStatus(SteamId32To64(playerSteamId32));
+                var vipStatus = CheckVipStatus(playerSteamId32);
 
-                SendMessageToDiscord(GetWebhook(), _PlayerName, _PlayerSteamId32, _AdminStatus, _VIPStatus);
+                Logger.LogInformation($"NAME = {playerName}");
+                Logger.LogInformation($"ID32 = {playerSteamId32}");
+                Logger.LogInformation($"ID64 = {playerSteamId64}");
+
+                _playerConnectTimes[playerSteamId32] = DateTime.Now;
+
+                SendToDiscord(_config.ConnectWebhookUrl, "Игрок подключился.", playerName, playerSteamId32, adminStatus, vipStatus);
             }
             else
             {
                 Logger.LogInformation("Ошибка при подключении игрока");
             }
+            return HookResult.Continue;
         }
-        return HookResult.Continue;
-    }
 
-    public string CheckAdminStatus(long sid)
-    {
-        Logger.LogInformation($"айдишник типа для бд - {sid}");
-        var builder = new MySqlConnectionStringBuilder
+        public void OnClientDisconnect(int playerSlot)
         {
-            Server = _config.LKSAdminHost,
-            UserID = _config.LKSAdminUser,
-            Password = _config.LKSAdminPassword,
-            Database = _config.LKSAdmindbName,
-        };
+            var player = Utilities.GetPlayerFromSlot(playerSlot);
 
-        using var connection = new MySqlConnection(builder.ConnectionString);
-        connection.Open();
-        try
+            if (player != null)
+            {
+                var playerName = player.PlayerName;
+                var playerSteamId32 = (int)player.SteamID;
+                var playerSteamId64 = player.SteamID;
+                var adminStatus = CheckAdminStatus(SteamId32To64(playerSteamId32));
+                var vipStatus = CheckVipStatus(playerSteamId32);
+
+                Logger.LogInformation($"NAME = {playerName}");
+                Logger.LogInformation($"ID32 = {playerSteamId32}");
+                Logger.LogInformation($"ID64 = {playerSteamId64}");
+
+                if (_playerConnectTimes.TryGetValue(playerSteamId32, out var connectTime))
+                {
+                    var playTime = DateTime.Now - connectTime;
+                    _playerConnectTimes.Remove(playerSteamId32);
+                    SendToDiscord(_config.DisconnectWebhookUrl, "Игрок отключился.", playerName, playerSteamId32, adminStatus, vipStatus, playTime);
+                }
+                else
+                {
+                    Logger.LogInformation("Не удалось определить время подключения игрока");
+                    SendToDiscord(_config.DisconnectWebhookUrl, "Игрок отключился.", playerName, playerSteamId32, adminStatus, vipStatus);
+                }
+            }
+            else
+            {
+                Logger.LogInformation("Ошибка при отключении игрока");
+            }
+        }
+
+        private string CheckAdminStatus(long sid)
         {
-            using var command = connection.CreateCommand();
-            command.CommandText = @"                    
+            Logger.LogInformation($"айдишник типа для бд - {sid}");
+            var builder = new MySqlConnectionStringBuilder
+            {
+                Server = _config.LKSAdminHost,
+                UserID = _config.LKSAdminUser,
+                Password = _config.LKSAdminPassword,
+                Database = _config.LKSAdmindbName,
+            };
+
+            using var connection = new MySqlConnection(builder.ConnectionString);
+            connection.Open();
+            try
+            {
+                using var command = connection.CreateCommand();
+                command.CommandText = @"
                     SELECT g.name
                     FROM iks_admins a
                     JOIN iks_groups g ON a.group_id = g.id
                     WHERE a.sid = @Sid;";
-            command.Parameters.AddWithValue("@Sid", sid);
+                command.Parameters.AddWithValue("@Sid", sid);
 
-            using var reader = command.ExecuteReader();
-            if (reader.Read())
-            {
-                string groupname = reader.GetString("name");
-                Logger.LogInformation("Ответ от бд - " + groupname);
-                return groupname;
-            }
-            else
-            {
-                Logger.LogInformation("Ответ от бд - " + "No results found");
-                return "Нет админ привелегии";
-            }
-        }
-        catch (Exception ex)
-        {
-            Logger.LogInformation("Ответ от бд - " + ex.ToString());
-            return $"{ex}";
-        }
-    }
-    public string CheckVipStatus(long sid)
-    {
-        Logger.LogInformation($"айдишник типа для бд - {sid}");
-        var builder = new MySqlConnectionStringBuilder
-        {
-            Server = _config.LKSAdminHost,
-            UserID = _config.LKSAdminUser,
-            Password = _config.LKSAdminPassword,
-            Database = _config.LKSAdmindbName,
-        };
-
-        using var connection = new MySqlConnection(builder.ConnectionString);
-        connection.Open();
-        try
-        {
-            using var command = connection.CreateCommand();
-            command.CommandText = @"SELECT `group` FROM `vip_users` WHERE `account_id` = @Sid";
-            command.Parameters.AddWithValue("@Sid", sid);
-
-            using var reader = command.ExecuteReader();
-            if (reader.Read())
-            {
-                string groupname = reader.GetString("group");
-                Logger.LogInformation("Ответ от бд - " + groupname);
-                return groupname;
-            }
-            else
-            {
-                Logger.LogInformation("Ответ от бд - " + "No results found");
-                return "Нет вип привелегии";
-            }
-        }
-        catch (Exception ex)
-        {
-            Logger.LogInformation("Ответ от бд - " + ex.ToString());
-            return $"{ex}";
-        }
-    }
-    public async void SendMessageToDiscord(string WebhookUrl, string PlayerName, int PlayerId, string AdminStatus, string VIPStatus)
-    {
-        Logger.LogInformation("Пытаюсь отправить сообщение в дискорд");
-        try
-        {
-            using var httpClient = new HttpClient();
-            var payload = JsonSerializer.Serialize(new
-            {
-                embeds = new[]
+                using var reader = command.ExecuteReader();
+                if (reader.Read())
                 {
+                    var groupname = reader.GetString("name");
+                    Logger.LogInformation("Ответ от бд - " + groupname);
+                    return groupname;
+                }
+                else
+                {
+                    Logger.LogInformation("Ответ от бд - No results found");
+                    return "Нет админ привелегии";
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogInformation("Ответ от бд - " + ex.ToString());
+                return $"{ex}";
+            }
+        }
+
+        private string CheckVipStatus(long sid)
+        {
+            Logger.LogInformation($"айдишник типа для бд - {sid}");
+            var builder = new MySqlConnectionStringBuilder
+            {
+                Server = _config.LKSAdminHost,
+                UserID = _config.LKSAdminUser,
+                Password = _config.LKSAdminPassword,
+                Database = _config.LKSAdmindbName,
+            };
+
+            using var connection = new MySqlConnection(builder.ConnectionString);
+            connection.Open();
+            try
+            {
+                using var command = connection.CreateCommand();
+                command.CommandText = "SELECT `group` FROM `vip_users` WHERE `account_id` = @Sid";
+                command.Parameters.AddWithValue("@Sid", sid);
+
+                using var reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    var groupname = reader.GetString("group");
+                    Logger.LogInformation("Ответ от бд - " + groupname);
+                    return groupname;
+                }
+                else
+                {
+                    Logger.LogInformation("Ответ от бд - No results found");
+                    return "Нет вип привелегии";
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogInformation("Ответ от бд - " + ex.ToString());
+                return $"{ex}";
+            }
+        }
+
+        private async void SendToDiscord(string webhookUrl, string title, string playerName, int playerId, string adminStatus, string vipStatus, TimeSpan? playTime = null)
+        {
+            Logger.LogInformation("Пытаюсь отправить сообщение в дискорд");
+            try
+            {
+                using var httpClient = new HttpClient();
+                var playTimeString = playTime.HasValue ? $"**Наиграное время:** {playTime.Value:hh\\:mm\\:ss}\n" : "";
+                var payload = JsonSerializer.Serialize(new
+                {
+                    embeds = new[]
+                    {
                         new
                         {
-                            title = "Игрок подключился.",
+                            title = title,
                             description = "Discord Pro Connection Logs",
                             color = 255,
                             fields = new[]
@@ -149,83 +192,85 @@ public class HelloWorldPlugin : BasePlugin
                                 new
                                 {
                                     name = "Игрок:",
-                                    value = 
-                                    $"**Имя:** {PlayerName}\n" +
-                                    $"**SteamID:** {SteamId32To64(PlayerId)}\n" +
-                                    $"**Админка** {AdminStatus}\n" +
-                                    $"**VIP** {VIPStatus}\n" +
-                                    $"**Ссылка:** [кликни, чтобы открыть стим](https://steamcommunity.com/profiles/{SteamId32To64(PlayerId)})",
+                                    value = $"**Имя:** {playerName}\n" +
+                                            $"**SteamID:** {SteamId32To64(playerId)}\n" +
+                                            $"**Админка:** {adminStatus}\n" +
+                                            $"**VIP:** {vipStatus}\n" +
+                                            playTimeString +
+                                            $"**Ссылка:** [кликни, чтобы открыть стим](https://steamcommunity.com/profiles/{SteamId32To64(playerId)})",
                                     inline = false
                                 }
                             }
                         }
                     }
-            });
-            var content = new StringContent(payload, Encoding.UTF8, "application/json");
-            var response = await httpClient.PostAsync(WebhookUrl, content);
+                });
+                var content = new StringContent(payload, Encoding.UTF8, "application/json");
+                await httpClient.PostAsync(webhookUrl, content);
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e.ToString());
+                throw;
+            }
         }
-        catch (Exception e)
+
+        private Config CreateConfig(string configPath)
         {
-            Console.WriteLine(e);
-            throw;
+            var config = new Config
+            {
+                ConnectWebhookUrl = "",
+                DisconnectWebhookUrl = "",
+                LKSAdminHost = "",
+                LKSAdminUser = "",
+                LKSAdminPassword = "",
+                LKSAdmindbName = "",
+                PisexVipHost = "",
+                PisexVipUser = "",
+                PisexVipdbName = "",
+                PisexVipPassword = "",
+            };
+
+            File.WriteAllText(configPath,
+                JsonSerializer.Serialize(config, new JsonSerializerOptions { WriteIndented = true }));
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("[ReportSystem] Конфиг успешно сохранён: " + configPath);
+            Console.ResetColor();
+
+            return config;
         }
-    }
-    private Config CreateConfig(string configPath)
-    {
-        var config = new Config
+
+        private Config LoadConfig()
         {
-            WebhookUrl = "",
-            LKSAdminHost = "",
-            LKSAdminUser = "",
-            LKSAdminPassword = "",
-            LKSAdmindbName = "",
-            PisexVipHost = "",
-            PisexVipUser = "",
-            PisexVipdbName = "",
-            PisexVipPassword = "",
-        };
+            var configPath = Path.Combine(ModuleDirectory, "settings.json");
 
-        File.WriteAllText(configPath,
-            JsonSerializer.Serialize(config, new JsonSerializerOptions { WriteIndented = true }));
+            if (!File.Exists(configPath)) return CreateConfig(configPath);
 
-        Console.ForegroundColor = ConsoleColor.DarkGreen;
-        Console.WriteLine("[ReportSystem] Конфиг успешно сохранён: " + configPath);
-        Console.ResetColor();
+            return JsonSerializer.Deserialize<Config>(File.ReadAllText(configPath))!;
+        }
 
-        return config;
-    }
-    private Config LoadConfig()
-    {
-        var configPath = Path.Combine(ModuleDirectory, "settings.json");
+        private static long SteamId32To64(int steamId)
+        {
+            return steamId | 76561197960265728L;
+        }
 
-        if (!File.Exists(configPath)) return CreateConfig(configPath);
+        private static int SteamId64To32(long steamId)
+        {
+            return (int)((steamId << 32) >> 32);
+        }
 
-        var config = JsonSerializer.Deserialize<Config>(File.ReadAllText(configPath))!;
-
-        return config;
-    }
-    static long SteamId32To64(int steamId)
-    {
-        return steamId | 76561197960265728L;
-    }
-    static int SteamId64To32(long steamId)
-    {
-        return (int)((steamId << 32) >> 32);
-    }
-    private string GetWebhook()
-    {
-        return _config.WebhookUrl;
-    }
-    public class Config
-    {
-        public required string WebhookUrl { get; set; }
-        public required string LKSAdminHost { get; set; }
-        public required string LKSAdminUser { get; set; }
-        public required string LKSAdmindbName { get; set; }
-        public required string LKSAdminPassword { get; set; }
-        public required string PisexVipHost { get; set; }
-        public required string PisexVipUser { get; set; }
-        public required string PisexVipPassword { get; set; }
-        public required string PisexVipdbName { get; set; }
+        public class Config
+        {
+            public required string ConnectWebhookUrl { get; set; }
+            public required string DisconnectWebhookUrl { get; set; }
+            public required string LKSAdminHost { get; set; }
+            public required string LKSAdminUser { get; set; }
+            public required string LKSAdmindbName { get; set; }
+            public required string LKSAdminPassword { get; set; }
+            public required string PisexVipHost { get; set; }
+            public required string PisexVipUser { get; set; }
+            public required string PisexVipPassword { get; set; }
+            public required string PisexVipdbName { get; set; }
+        }
     }
 }
